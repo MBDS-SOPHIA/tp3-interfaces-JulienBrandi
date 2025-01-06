d681463b7ff9115efa0dad2a1917faedacc3086a
JulienBrandi <julien.brandi@etu.unice.fr>
2025-01-06 12:03:03 +0100

    Etape 4, un peu mieux

--- app/src/main/java/com/jbrandi/TP3/MainActivity.kt
+++ app/src/main/java/com/jbrandi/TP3/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.jbrandi.TP3
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -19,18 +23,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+
+        val inputText: EditText = findViewById(R.id.input)
+        inputText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                rollDice()
+            }
+        })
     }
 
     private fun rollDice() {
         val textResult: TextView = findViewById(R.id.result)
-        val textInput: TextView = findViewById(R.id.input)
-        val inputValue = textInput.text.toString()
+        val textInput: EditText = findViewById(R.id.input)
+        val inputValue = textInput.text.toString().toIntOrNull()
         
-        if (inputValue.toInt() < 2 || inputValue.toInt() > 12) {
+        if (inputValue == null || inputValue < 2 || inputValue > 12) {
             textResult.text = "Enter a number between 2 and 12"
             return
         }
@@ -49,12 +60,28 @@ class MainActivity : AppCompatActivity() {
         textView2.text = diceRoll2.toString()
 
 
-        if (diceRollResults == inputValue.toInt()) {
+        if (diceRollResults == inputValue) {
             textResult.text = "Congrats! You win"
+            animateDice(textView, textView2)
         } else {
             textResult.text = "Try again"
         }
 
+    }
+
+    private fun animateDice(dice1: TextView, dice2: TextView) {
+        val animator1 = ObjectAnimator.ofFloat(dice1, "translationY", -100f, 0f)
+        animator1.duration = 500
+        animator1.repeatCount = 3
+        animator1.repeatMode = ObjectAnimator.REVERSE
+
+        val animator2 = ObjectAnimator.ofFloat(dice2, "translationY", -100f, 0f)
+        animator2.duration = 500
+        animator2.repeatCount = 3
+        animator2.repeatMode = ObjectAnimator.REVERSE
+
+        animator1.start()
+        animator2.start()
     }
 }
 
